df46849280b601bfc82a9782eb7ce11b3de36598
Andrew Pham <email": null>
2015-08-24 12:55:00 -0700

    set model associations. add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+sub = Subject.create(title: "backbone.js")
+sub.notes.create(content: "Keeping track of all the things")
+sub.notes.create(content: "Is hard to do")
+
+sub1 = Subject.create(title: "Ruby on Rails")
+sub1.notes.create(content: "Little easier to handle")
